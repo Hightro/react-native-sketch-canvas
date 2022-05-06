c0dad4aa4bf67f532d0ea764f2250fefd9e0d8c3
Joshua Nicholl <josh@usesoftware.com.au>
2022-05-06 15:59:12 +1000

    change types to allow empty filename and folder

--- index.d.ts
+++ index.d.ts
@@ -99,7 +99,7 @@ export class SketchCanvas extends React.Component<SketchCanvasProps & ViewProper
    * @param includeText Set to `true` to include the text drawn from `Text`.
    * @param cropToImageSize Set to `true` to crop output image to the image loaded from `LocalSourceImage`
    */
-  save(imageType: ImageType, transparent: boolean, folder: string, filename: string, includeImage: boolean, includeText: boolean, cropToImageSize: boolean): void
+  save(imageType: ImageType, transparent: boolean, folder?: string, filename?: string, includeImage?: boolean, includeText?: boolean, cropToImageSize?: boolean): void
   getPaths(): Path[]
 
   /**
